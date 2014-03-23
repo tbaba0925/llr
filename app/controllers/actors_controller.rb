907888b41168d33f4793dffdb0a56d61a0297afe
tbaba <t.baba@aktsk.jp>
2014-03-23 15:03:04 +0900

    WIP

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,6 +1,11 @@
 class ActorsController < ApplicationController
   def index
     MyLight.alert = "select"
-    @color = params[:color]
+    @color = params[:color] || "no color"
+  end
+
+  def show
+    MyLight.alert = "select"
+    @color = params[:color] || "no color"
   end
 end
